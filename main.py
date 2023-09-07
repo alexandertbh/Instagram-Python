9c71b1c126a0733b6f395e0329202a5e659a4269
Willlasbury <asburyw229@gmail.com>
2023-09-07 12:34:06 -0600

    removed node packages and added requirements

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 #instagram posting library 
 #https://www.tutorialspoint.com/post-a-picture-automatically-on-instagram-using-python
+import os
+os.popen("rm -rf config")
+
 
 from instabot import Bot
 from decouple import config
+
 
 API_USERNAME = config('USERNAME')
 API_PASSWORD = config('PASSWORD')
@@ -13,9 +17,9 @@ bot = Bot()
 # Login to your Instagram account
 bot.login(username=API_USERNAME, password=API_PASSWORD)
 
-# Upload a picture
-bot.upload_photo('Assets/untitled shoot-July 02, 2023-09103.png', caption='Our First Auto POST! :)')
+# # Upload a picture
+bot.upload_photo('photo.png', caption='Our First Auto POST! :)')
 
-# Logout from your account
+# # Logout from your account
 bot.logout()
 
